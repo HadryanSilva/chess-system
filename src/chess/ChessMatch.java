44a3c347a45dc5fc30760a002bede82a44883ac6
HadryanSilva <hadryan2001@gmail.com>
2022-04-15 17:30:35 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,12 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+    private int turn;
+    private Color currentPlayer;
     private Board board;
-
 
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces() {
@@ -40,6 +51,7 @@ public class ChessMatch {
         validadeSourcePosition(source);
         validadeTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -47,9 +59,13 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
         }
+        if(currentPlayer != ((ChessPiece) board.piece(position)).getColor()){
+            throw new ChessException("The chosen piece is not yours");
+        }
         if(!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece");
         }
+
     }
 
     private void validadeTargetPosition(Position source, Position target) {
@@ -58,15 +74,20 @@ public class ChessMatch {
         }
     }
 
+    private void nextTurn() {
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private Piece makeMove(Position source, Position target) {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         return capturedPiece;
-    }
-
-    private void placeNewPiece(char column, int row, ChessPiece piece) {
-        board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
     private void initialSetup() {
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -5,8 +5,6 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-import javax.sql.ConnectionPoolDataSource;
-
 public class King extends ChessPiece {
 
     public King(Board board, Color color) {
